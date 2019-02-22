bdca77f2d41da53928f8175f97d3b14dee6f7f92
hyungjinkim <hjkim0508@gmail.com>
2019-02-22 20:08:19 +0900

    remove pclomp

--- src/pcl_omp/src/gicp_omp.cpp
+++
@@ -1,6 +0,0 @@
-#include "gicp_omp.h"
-#include "gicp_omp_impl.hpp"
-
-template class pclomp::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>;
-template class pclomp::GeneralizedIterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI>;
-
--- src/pcl_omp/src/ndt_omp.cpp
+++
@@ -1,5 +0,0 @@
-#include "ndt_omp.h"
-#include "ndt_omp_impl.hpp"
-
-template class pclomp::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>;
-template class pclomp::NormalDistributionsTransform<pcl::PointXYZI, pcl::PointXYZI>;
